8891c068ec50a78c9b5b8f45849b0f0ad6335c9d
ulfahnh03 <ulfahnh03@gmail.com>
2024-07-04 05:12:26 +0000

    up

--- lib/pages/about_page.dart
+++ lib/pages/about_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class AboutPage extends StatelessWidget {
@@ -8,15 +7,60 @@ class AboutPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
+        title: const Text(
           "About app",
           style: TextStyle(
-            fontWeight: FontWeight.bold
+            fontWeight: FontWeight.bold,
           ),
         ),
       ),
-      body: Center(
-        child: const Text("itg.ac.id © 2024"),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Profile picture
+            CircleAvatar(
+              radius: 60,
+              backgroundImage: AssetImage(
+                  'assets/images/liu.jpg'), // replace with your image link or asset path
+            ),
+            SizedBox(height: 20),
+            // Name
+            Text(
+              "Ulfah Nurhasanah",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 5),
+            // Job Title
+            Text(
+              "Admin Page",
+              style: TextStyle(
+                fontSize: 16,
+                color: Color.fromARGB(255, 83, 123, 135),
+              ),
+            ),
+            SizedBox(height: 40),
+            // Additional text
+            Text(
+              "notesappmobile",
+              style: TextStyle(
+                fontSize: 16,
+                color: Color.fromARGB(255, 0, 0, 0),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              "©2024 itg.ac.id",
+              style: TextStyle(
+                fontSize: 18,
+                color: Color.fromARGB(255, 43, 166, 214),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
